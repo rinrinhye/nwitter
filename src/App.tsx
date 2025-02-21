5e2a8c514d6a6c4e31b522e8fc2d6751cb28f62b
rinrinhye <hyerin98323@gmail.com>
2025-02-21 15:14:46 +0900

    2.1 rounting - done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./routes/Home";
 import Profile from "./routes/Profile";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/CreateAccount";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+	${reset}
+	body{
+		background: black;
+		color: white;
+	}
+	*{box-sizing : border-box;}
+`;
 
 const router = createBrowserRouter([
 	{
@@ -18,11 +31,20 @@ const router = createBrowserRouter([
 			},
 		],
 	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/create-account",
+		element: <CreateAccount />,
+	},
 ]);
 
 function App() {
 	return (
 		<>
+			<GlobalStyles />
 			<RouterProvider router={router} />
 		</>
 	);
---
+++ src/routes/CreateAccount.tsx
@@ -0,0 +1,3 @@
+export default function CreateAccount() {
+	return <h1>가입</h1>;
+}
